e1e8c536664120cfd39644ee295b2d5800002bef
Rober Guerra <roberzguerra@gmail.com>
2018-11-05 17:41:07 -0200

    Exemplo view e template.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+
+def list(request):
+    #question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'blog/list.html', {'item': 'Testeee....'})

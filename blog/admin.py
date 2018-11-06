89dc8c29da5b79bcfc22903122c1ab15cfe544c9
Rober Guerra <roberzguerra@gmail.com>
2018-11-05 22:28:13 -0200

    Model Post para o blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from blog.models import Category
+from blog.models import Category, Post
 
 
 class CategoryAdmin(admin.ModelAdmin):
     """
     Configurações do admin do model Category
     """
-    fields = ('name', 'description', )
-    list_display = ('name', 'get_created_at', 'updated_at', )
+    fields = ('name', 'description', ) # colunas exibidas no formulario do admin
+    list_display = ('name', 'get_created_at', 'updated_at', ) # Colunas listadas no admin
+
+    def get_created_at(self, obj):
+        return obj.created_at.strftime("%d/%m/%Y %H:%M:%S")
+    get_created_at.short_description = "Criado em"
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    Configurações do admin do model Post
+    """
+    fields = ('title', 'content', 'category', ) # colunas exibidas no formulario do admin
+    list_display = ('title', 'category', 'get_created_at', 'updated_at', ) # Colunas listadas no admin
 
     def get_created_at(self, obj):
         return obj.created_at.strftime("%d/%m/%Y %H:%M:%S")
@@ -16,3 +29,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
